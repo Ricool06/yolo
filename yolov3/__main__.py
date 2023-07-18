5599de1184916ed2b7a0bcc0ed9c5dee8ca473ee
Ricool <ricoolohsix@hotmail.com>
2023-07-19 00:29:49 +0100

    exp: experiment with Conv2d layers

--- yolov3/__main__.py
+++ yolov3/__main__.py
@@ -1,63 +1,147 @@
-import numpy
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
 import sys
+from typing import TypedDict
+from torch import Tensor
+import torch
+from torchvision import datasets
+from torchvision.transforms import Compose, ToTensor, CenterCrop, Lambda
+from torchvision.transforms.functional import to_tensor, center_crop, to_pil_image
+from PIL.JpegImagePlugin import JpegImageFile
+from PIL import ImageDraw
+from torch.utils.data import DataLoader
+from torch import nn
 
 
-classes = [
-    "T-shirt/top",
-    "Trouser",
-    "Pullover",
-    "Dress",
-    "Coat",
-    "Sandal",
-    "Shirt",
-    "Sneaker",
-    "Bag",
-    "Ankle boot",
-]
+def draw_test_image(data: datasets.VOCDetection):
+    image_tensor: Tensor
+    target: Tensor
+
+    for image_tensor, target in data:
+        img = to_pil_image(image_tensor)
+
+        draw = ImageDraw.Draw(img)
+        x, y, w, h = target.flatten().tolist()
+        rect = (x - w / 2, y - h / 2, x + w / 2, y + h / 2)
+        draw.rectangle(rect, outline=(40, 180, 80, 40), width=4)
+
+        img.save("test.jpeg")
+        break
 
 
-def load_fashion_data():
-    training_data = datasets.FashionMNIST(
-        root="data", train=True, download=True, transform=ToTensor()
+def equal_size_transforms(input, target):
+    desired_size = [600, 600]
+    original_size = [
+        int(target["annotation"]["size"]["width"]),
+        int(target["annotation"]["size"]["height"]),
+    ]
+    left_pad = (desired_size[0] - original_size[0]) / 2
+    top_pad = (desired_size[1] - original_size[1]) / 2
+
+    tensor_input = to_tensor(input)
+    cropped_input = center_crop(tensor_input, desired_size)
+    # print(f"{cropped_input.size()=}")
+
+    bounding_boxes = []
+    for obj in target["annotation"]["object"]:
+        bbox = obj["bndbox"]
+        xmin, ymin, xmax, ymax = (
+            int(bbox["xmin"]) + left_pad,
+            int(bbox["ymin"]) + top_pad,
+            int(bbox["xmax"]) + left_pad,
+            int(bbox["ymax"]) + top_pad,
+        )
+        width = xmax - xmin
+        height = ymax - ymin
+        yolo_bbox = [xmin + width / 2, ymin + height / 2, width, height]
+        bounding_boxes.append(yolo_bbox)
+        break
+
+    tensor_bbox_target = Tensor(bounding_boxes)
+    # print(f"{tensor_bbox_target.size()=}")
+    return cropped_input, tensor_bbox_target
+
+
+def load_data():
+    train_data = datasets.VOCDetection(
+        "data",
+        year="2012",
+        image_set="train",
+        download=False,
+        transforms=equal_size_transforms,
+    )
+    val_data = datasets.VOCDetection(
+        "data",
+        year="2012",
+        image_set="trainval",
+        download=False,
+        transforms=equal_size_transforms,
+    )
+    test_data = datasets.VOCDetection(
+        "data",
+        year="2012",
+        image_set="val",
+        download=False,
+        transforms=equal_size_transforms,
     )
 
-    test_data = datasets.FashionMNIST(
-        root="data", train=False, download=True, transform=ToTensor()
-    )
+    if "--draw-test" in sys.argv:
+        draw_test_image(val_data)
 
     batch_size = 64
 
-    training_dataloader = DataLoader(training_data, batch_size=batch_size)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    return (
+        DataLoader(train_data, batch_size=batch_size),
+        DataLoader(val_data, batch_size=batch_size),
+        test_data,
+    )
 
-    return (training_dataloader, test_dataloader, training_data, test_data)
 
-
-class MyNeuralNetwork(nn.Module):
-    """NeuralNetwork defines a basic pytorch neural network."""
+class Yolo(nn.Module):
+    """Yolo defines a basic pytorch YOLOv3 neural network."""
 
     def __init__(self, device: str):
-        super(MyNeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28 * 28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
+        super(Yolo, self).__init__()
+
+        kernel_size = 3
+
+        # self.layer = nn.Conv2d(
+        #     in_channels=3,
+        #     out_channels=64,
+        #     kernel_size=kernel_size,
+        #     stride=(1, 1),
+        #     padding=(kernel_size / 2, kernel_size / 2),
+        # )
+        self.layer = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=3,
+                kernel_size=3,
+            ),
+            nn.LeakyReLU(),
+            # nn.Conv2d(
+            #     in_channels=3,
+            #     out_channels=9,
+            #     kernel_size=9,
+            #     stride=3
+            # ),
+            # nn.LeakyReLU(),
+            # nn.Conv2d(
+            #     in_channels=9,
+            #     out_channels=81,
+            #     kernel_size=81,
+            #     stride=9
+            # ),
+            # nn.LeakyReLU(),
+            # nn.Conv2d(
+            #     in_channels=81,
+            #     out_channels=3,
+            #     kernel_size=3
+            # ),
         )
+
         self.device = device
 
     def forward(self, x):
-        flattened = self.flatten(x)
-        logits = self.linear_relu_stack(flattened)
-        return logits
+        return self.layer(x)
 
 
 def make_model():
@@ -71,109 +155,27 @@ def make_model():
 
     print(device)
 
-    model = MyNeuralNetwork(device).to(device)
+    model = Yolo(device).to(device)
     print(model)
     return model
 
 
-def create_loss_fn_and_optimiser(model: MyNeuralNetwork):
-    return (nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr=1e-3))
-
-
-def train(
-    dataloader: DataLoader,
-    model: MyNeuralNetwork,
-    loss_fn: nn.CrossEntropyLoss,
-    optimiser: torch.optim.SGD,
-):
-    dataset_size = len(dataloader.dataset)
-    model.train()
-
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(model.device), y.to(model.device)
-
-        prediction = model(X)
-        loss = loss_fn(prediction, y)
-
-        loss.backward()
-        optimiser.step()
-        optimiser.zero_grad()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"{loss=} [{current:>5d}/{dataset_size}]")
-
-
-def test(
-    dataloader: DataLoader,
-    model: MyNeuralNetwork,
-    loss_fn: nn.CrossEntropyLoss,
-):
-    dataset_size = len(dataloader.dataset)
-    number_of_batches = len(dataloader)
-    model.eval()
-
-    loss, correct = 0, 0
-
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(model.device), y.to(model.device)
-
-            prediction = model(X)
-            loss = loss_fn(prediction, y).item()
-
-            correct += (prediction.argmax(1) == y).type(torch.float).sum().item()
-
-    loss /= number_of_batches
-    correct /= dataset_size
-
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {loss:>8f} \n")
-
-
-def eval(
-    test_data: datasets.FashionMNIST,
-    model: MyNeuralNetwork,
-    i: int,
-):
-    model.eval()
-
-    X, y = test_data[i][0], test_data[i][1]
-
-    with torch.no_grad():
-        X = X.to(model.device)
-        prediction = model(X)
-        predicted, actual = classes[prediction[0].argmax(0)], classes[y]
-
-        return predicted, actual
-
-
 if __name__ == "__main__":
-    training_dataloader, test_dataloader, training_data, test_data = load_fashion_data()
+    train_loader, val_loader, test_data = load_data()
     model = make_model()
-    loss_fn, optimiser = create_loss_fn_and_optimiser(model)
 
-    if "--train" in sys.argv:
-        for epoch in range(20):
-            print(f"---- {epoch=} ----\n")
-            train(training_dataloader, model, loss_fn, optimiser)
+    with torch.no_grad():
+        for image_tensor_batch, target in val_loader:
+            print(f"{image_tensor_batch.size()=}")
+            
+            out = model(image_tensor_batch.to(model.device))
 
-            test(test_dataloader, model, loss_fn)
+            for i in range(5):
+                in_img = to_pil_image(image_tensor_batch[i])
+                in_img.save(f"test_images/in{i}.jpeg")
+                
+                out_img = to_pil_image(out[i])
+                out_img.save(f"test_images/out{i}.jpeg")
 
-        torch.save(model.state_dict(), "model.pth")
-    else:
-        model.load_state_dict(torch.load("model.pth"))
-
-        plt.style.use("grayscale")
-        plt.axis("off")
-        figure = plt.figure(figsize=(25, 25))
-
-        for i in range(25):
-            predicted, actual = eval(test_data, model, i)
-            figure.add_subplot(5, 5, i + 1, frameon=False)
-            plt.xlabel(f"{predicted=} {actual=}")
-            plt.imshow(numpy.reshape(test_data[i][0], (28, 28)))
-
-        figure.tight_layout()
-        plt.savefig(f"graph.png")
-
-    print("Done.")
+            
+            break
